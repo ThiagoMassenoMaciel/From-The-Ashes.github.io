46a3019357f2cd5777491500630d0b426003e9e3
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2024-03-05 15:09:43 -0300

    test 1 do step 1

--- Desafios.md
+++ Desafios.md
@@ -5,4 +5,9 @@
 #### 2 -transformar matriz em labirinto
 ![image](https://github.com/ThiagoMassenoMaciel/From-The-Ashes.github.io/assets/107934374/97b78101-0721-4a35-b2ce-9cf5c97acd0d)
 #### 1.1 `Armazenar uma matriz com estes elementos em uma variavel`
+#### 1.2 `Percorrer cada elemento da matriz e fazer o teste`
+##### --Se o elemento for 1 isso significa que no labirinto vou adicionar um bloco-pareide que corresponde a linha e coluna deste elemento
+##### ---chamar a função que coloca a imagem(bloco pereide) no canvas do jogador passando por parametro a linha e coluna deste elemento da matriz
+##### --SE o elemento for 0 isso significa que no labirinto vou adicionar um bloco-espaço que corresponde a linha e coluna deste elemento
+##### ---chamar a função que coloca a imagem(bloco espaço) no canvas do jogador passando por parametro a linha e coluna deste elemento da matriz
 
--- desafio1.js
+++ desafio1.js
@@ -7,9 +7,20 @@ const matriz = [
 ]
 
 function exibirMatriz() {
+  const branco = 'bloco-espaço'
+  const preto  = 'bloco-pareide'
+
   for (let linha = 0; linha < matriz.length; linha++) {
+
       for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
-          document.write(matriz[linha][coluna] + " ");
+          //document.write(matriz[linha][coluna] + " ");
+
+        if(matriz[linha][coluna] === 1){
+          preencherBlocoNoCanvas( linha , coluna , preto)
+        }else{
+          preencherBlocoNoCanvas( linha , coluna , branco)
+        }
+
       }
       document.write("<br>");
   }
@@ -17,3 +28,8 @@ function exibirMatriz() {
 
 // Chame a função para exibir a matriz
 exibirMatriz();
+
+function preencherBlocoNoCanvas( linha , coluna , id_bloco){
+  //this.load.image('red', 'assets/particles/red.png');  a imagem ja tem que estar preload 
+  this.add.image(coluna , linha, id_bloco);
+}
