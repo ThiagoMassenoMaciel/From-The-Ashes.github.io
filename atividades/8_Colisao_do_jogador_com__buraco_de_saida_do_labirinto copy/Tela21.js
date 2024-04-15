b7f598fecbf27c98b8f4a12a1ac93e5b7f8c01cd
Thiago Masseno <thiagomasseno3@gmail.com>
2024-04-15 19:21:33 -0300

    1h10'      Atv 8 começei + bug

---
+++ atividades/8_Colisao_do_jogador_com__buraco_de_saida_do_labirinto copy/Tela21.js
@@ -0,0 +1,123 @@
+class Tela21 extends Phaser.Scene{
+  constructor(){
+    super("21")
+  }
+
+  create(){
+
+    this.add.text(0,0, "jogo", { font: "60px Arial"})
+    this.timer = this.add.image(config.width - 150 ,  100 ,"tempo")
+    this.timer.setScale(0.6)
+    this.button = this.add.image(config.width/2 , config.height - 80 ,"botao")
+    this.button.setScale(0.6)
+
+
+    const matrizes = []
+
+    //-------------------
+    //ponto inicial linha[4] coluna[4]
+    //ponto final   linha[0] coluna[0]
+    const easy_1 = [ 
+      [0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0],
+      [0, 0, 0, 0, 0],
+      [0, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0]
+    ]
+
+    matrizes.push(easy_1)
+
+    const matriz = matrizes[0]
+
+    this.todos_blocos_parede = this.physics.add.staticGroup();
+  
+    for (let linha = 0; linha < matriz.length; linha++) {
+
+      for (let coluna = 0; coluna < matriz[linha].length; coluna++) {
+            
+        console.log(` linha ${linha} coluna ${coluna}`)
+
+        if(matriz[linha][coluna] === 1){
+
+          this.todos_blocos_parede.create( coluna * 120 + (config.width/3) - 80 ,  linha * 120 ,"pareide").setOrigin(0,0).setScale(3).refreshBody();  
+        }else{
+
+          this.chao = this.add.image(coluna * 120  + (config.width/3) - 80 ,   linha * 120, "chao") 
+          this.chao.setOrigin(0,0)
+          this.chao.setScale(3)
+
+        }
+    
+      }
+          
+    }
+
+    this.todos_blocos_parede.create((config.width/3) - 80 - 10           ,  0              , "borda_left").setOrigin(0,0).refreshBody();
+    this.todos_blocos_parede.create( ( 5 * 120 ) + (config.width/3) - 80 ,  0              , "borda_right").setOrigin(0,0).refreshBody();
+    this.todos_blocos_parede.create( (config.width/3) - 80               , (5 * 120 )      , "borda_bottom").setOrigin(0,0).refreshBody(); 
+
+    const linhaI = 4
+    const colunaI = 4
+
+    const linhaF = 0
+    const colunaF = 0
+
+    this.saida = this.physics.add.sprite( colunaF * 120 + (config.width/3) - 79.5  , ( linhaF * 120 ) + 0.5,"saidaa").setOrigin(0,0).setScale(0.2).refreshBody();
+
+    //animando o sprite saida
+    this.anims.create( {
+
+      key: "saidaa_anims",
+      frames: this.anims.generateFrameNumbers("saidaa"),
+      frameRate: 4,
+      repeat: -1
+      
+    })
+    //executando animação
+    this.saida.anims.play("saidaa_anims", true);
+
+    this.jogadorr = this.physics.add.sprite(colunaI * 120 + (config.width/3) - 79.5  ,  (linhaI * 120 ) + 0.5,"jogador").setOrigin(0,0).setScale(0.2).refreshBody();
+    this.jogadorr.setCollideWorldBounds(true);
+
+    console.log("até aqui funcionou ?")
+    //animando o sprite teste = jogador
+
+    this.anims.create( {
+
+      key: "jogador_anims",
+      frames: this.anims.generateFrameNumbers("jogador"),
+      frameRate: 15,
+      repeat: -1
+      
+    })
+      //executando animação
+    this.jogadorr.anims.play("jogador_anims", true);
+
+    this.physics.add.collider(this.jogadorr, this.todos_blocos_parede);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    setTimeout( ()=>{this.scene.start("22")}, 10000)
+  }
+    
+  update(){
+    if (this.cursors.left.isDown) {
+      this.jogadorr.setVelocityX(-300);
+   }else if (this.cursors.right.isDown) {
+      this.jogadorr.setVelocityX(300);
+   }else{
+     this.jogadorr.setVelocityX(0)
+   }
+   
+   if (this.cursors.up.isDown) {
+     this.jogadorr.setVelocityY(-300);
+   }else  if( this.cursors.down.isDown){
+     this.jogadorr.setVelocityY(300)
+   }else{
+     this.jogadorr.setVelocityY(0)
+   }
+
+  }
+}
+
+
---
+++ atividades/8_Colisao_do_jogador_com__buraco_de_saida_do_labirinto copy/Tela42.js
@@ -0,0 +1,139 @@
+class Tela42 extends Phaser.Scene{
+  constructor(){
+    super("42")
+  }
+
+  create(){ //5h 24' até 6h25
+
+    this.add.text(5,5, "tela hard_2", {font: "50px Arial" , fill: 'white'})
+
+    this.timer = this.add.image(config.width - 150 ,  100 ,"tempo")
+    this.timer.setScale(0.6)
+    this.button = this.add.image(config.width/2 , config.height - 80 ,"botao")
+    this.button.setScale(0.6)
+
+    const matrizes = []
+
+    // Ponto inicial : linha[19] coluna[18]
+    // Ponto final : linha[0] coluna[0]
+    const hard_2 = [
+      [0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+      [1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+      [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+      [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+      [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+      [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+      [0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+      [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
+      [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+      [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+      [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1]
+    ]
+
+    matrizes.push(hard_2)
+
+
+    const matriz_hard = matrizes[0]
+
+    this.todos_blocos_parede = this.physics.add.staticGroup();
+
+    for (let linha = 0; linha < matriz_hard.length; linha++) {
+    
+      for (let coluna = 0; coluna < matriz_hard[linha].length; coluna++) {
+      
+        if(matriz_hard[linha][coluna] === 1){
+ 
+          this.todos_blocos_parede.create( coluna * 30 + (config.width/3) - 80 ,  linha * 30 ,"pareide").setOrigin(0,0).setScale(0.75).refreshBody();
+
+        }else{
+
+          this.chao = this.add.image(coluna * 30  + (config.width/3) - 80 ,   linha * 30, "chao").setOrigin(0,0).setScale(0.75)
+
+        }
+    
+      }
+        
+    }
+
+    this.todos_blocos_parede.create((config.width/3) - 80 - 10           ,  0         , "borda_left").setOrigin(0,0).refreshBody();
+    this.todos_blocos_parede.create( ( 20 * 30 ) + (config.width/3) - 80 ,  0         , "borda_right").setOrigin(0,0).refreshBody();
+    this.todos_blocos_parede.create( (config.width/3) - 80               , (20 * 30 ) , "borda_bottom").setOrigin(0,0).refreshBody();   
+
+    const linhaI = 19
+    const colunaI = 18
+
+    const linhaF = 0
+    const colunaF = 0
+
+    this.saida = this.physics.add.sprite( colunaF * 30 + (config.width/3) - 80  , ( linhaF * 30 ) ,"saidaa").setOrigin(0,0).setScale(0.050).refreshBody();
+
+    this.anims.create( {
+
+      key: "saidaa_anims",
+      
+      frames: this.anims.generateFrameNumbers("saidaa"),
+      
+      frameRate: 4,
+      
+      repeat: -1
+      
+    })
+    
+    this.saida.anims.play("saidaa_anims", true);
+
+    this.jogadorr = this.physics.add.sprite(colunaI * 30 + (config.width/3) - 80  ,  (linhaI * 30 ) ,"jogador").setOrigin(0,0).setScale(0.050).refreshBody(); // 29,85
+    this.jogadorr.setCollideWorldBounds(true);
+
+    this.anims.create( {
+
+      key: "jogador_anims",
+      frames: this.anims.generateFrameNumbers("jogador"),
+      frameRate: 25,
+      repeat: -1
+      
+    })
+
+    this.jogadorr.anims.play("jogador_anims", true);
+
+    this.physics.add.collider(this.jogadorr, this.todos_blocos_parede);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    setTimeout( ()=>{ this.scene.start("43")}, 20000)
+  }
+
+  update(){
+    if (this.cursors.left.isDown) {
+        this.jogadorr.setVelocityX(-150);
+
+    }else if (this.cursors.right.isDown) {
+        this.jogadorr.setVelocityX(150);
+
+    }else{
+      this.jogadorr.setVelocityX(0)
+    }
+    
+    if (this.cursors.up.isDown) {
+      this.jogadorr.setVelocityY(-150);
+
+    }else  if( this.cursors.down.isDown){
+      this.jogadorr.setVelocityY(150)
+
+    }else{
+      this.jogadorr.setVelocityY(0)
+
+    }
+  }
+    
+}
+
+
