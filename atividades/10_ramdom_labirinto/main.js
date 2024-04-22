c486838a6fae752316a9bb61ba0b5f8b48cdfe42
Thiago Masseno <thiagomasseno3@gmail.com>
2024-04-22 17:20:18 -0300

    solving some bugs

--- atividades/10_ramdom_labirinto/main.js
+++ atividades/10_ramdom_labirinto/main.js
@@ -3,7 +3,7 @@ var config ={
   width : 1364,
   height: 764,
   backgroundColor: 0xffffff,
-  scene: [  Tela1, Tela ,TelaMenu, Tela2, Tela3, Tela4, Tela31, Tela32, Tela33, Tela34, Tela41, Tela42, Tela43, Tela44],
+  scene: [  Tela1, Tela ,TelaMenu, Tela2, Tela3, Tela4, Tela41, Tela42, Tela43, Tela44],
   physics : {  
     default: "arcade", 
     arcade: {  
