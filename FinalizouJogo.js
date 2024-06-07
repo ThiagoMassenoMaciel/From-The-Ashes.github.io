131e5b4556fe0cb54228accbf2cef3f74ecd3a47
Thiago Masseno <thiagomasseno3@gmail.com>
2024-06-07 20:46:18 -0300

    + notes

--- FinalizouJogo.js
+++ FinalizouJogo.js
@@ -8,9 +8,11 @@ class FinalizouJogo extends Phaser.Scene{
     this.Aviso_fase_3_clicou_botao_proximo_ = this.add.image( config.width/2 , config.height/2 , "fase_3_clicou_botao_proximo" )
     this.Botao_Fase_voltar___ = this.add.image( config.width/2 , (config.height/2) + 130, "Botao_Fase_voltar_" ).setInteractive().on('pointerdown', () =>
       {
-          console.log('voltar para cena Tela2');
-          this.scene.start("TelaFases")
-
+        const music = this.sound.add("click");
+        music.play() 
+        console.log('voltar para cena Tela2');
+        this.scene.start("TelaFases")
+        
       });
 
     console.log("\n\n\n\n\n\n\n\n\n\n\namostrado aviso jogo finalizado ")
